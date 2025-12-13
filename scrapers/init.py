0e361ff4a149c68306293d61c1f2cd023621f2b8
YtalloCosta <164099807+YtalloCosta@users.noreply.github.com>
2025-12-13 20:35:44 -0300

    Update init.py

--- scrapers/init.py
+++ scrapers/init.py
@@ -1,1 +1,22 @@
-# package
+from .base import BaseScraper
+from .bet365 import Bet365Scraper
+from .bet365_template import Bet365Template
+from .betfair_template import BetfairTemplate
+from .betano import BetanoScraper
+from .betano_scraper import BetanoTemplate
+from .kto_scraper import KtoScraper
+from .mock_scraper import MockScraper
+from .pinnacle import PinnacleScraper
+from .playwright_template import PlaywrightTemplate
+from .sportingbet import SportingBetScraper
+from .sportingbet_scraper import SportingbetTemplate
+from .x1bet_template import OneXBetScraperTemplate
+
+SCRAPERS = [
+    Bet365Scraper(),
+    BetanoScraper(),
+    PinnacleScraper(),
+    SportingBetScraper(),
+    KtoScraper(),
+    # adicione outros aqui conforme forem ficando prontos
+]
