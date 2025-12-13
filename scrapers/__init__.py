ceb35d81a356581bbdf60afbbab983153296d658
YtalloCosta <164099807+YtalloCosta@users.noreply.github.com>
2025-12-13 20:53:13 -0300

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,14 +1,7 @@
 from .pinnacle import PinnacleScraper
 from .stake import StakeScraper
-from .xbet_1 import OneXBetScraper
-from .xbet_22 import TwentyTwoBetScraper
-from .bwin import BwinScraper
 
-# Lista oficial de scrapers ativos
 SCRAPERS = [
     PinnacleScraper(),
     StakeScraper(),
-    OneXBetScraper(),
-    TwentyTwoBetScraper(),
-    BwinScraper(),
 ]
