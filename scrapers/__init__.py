28659160549c7237d5eeee39342656a5829a3884
YtalloCosta <164099807+YtalloCosta@users.noreply.github.com>
2025-12-13 20:41:31 -0300

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,22 +1,14 @@
-from .base import BaseScraper
-from .bet365 import Bet365Scraper
-from .bet365_template import Bet365Template
-from .betfair_template import BetfairTemplate
-from .betano import BetanoScraper
-from .betano_scraper import BetanoTemplate
-from .kto_scraper import KtoScraper
-from .mock_scraper import MockScraper
 from .pinnacle import PinnacleScraper
-from .playwright_template import PlaywrightTemplate
-from .sportingbet import SportingBetScraper
-from .sportingbet_scraper import SportingbetTemplate
-from .x1bet_template import OneXBetScraperTemplate
+from .stake import StakeScraper
+from .xbet_1 import OneXBetScraper
+from .xbet_22 import TwentyTwoBetScraper
+from .bwin import BwinScraper
 
+# Lista oficial de scrapers ativos
 SCRAPERS = [
-    Bet365Scraper(),
-    BetanoScraper(),
     PinnacleScraper(),
-    SportingBetScraper(),
-    KtoScraper(),
-    # adicione outros aqui conforme forem ficando prontos
+    StakeScraper(),
+    OneXBetScraper(),
+    TwentyTwoBetScraper(),
+    BwinScraper(),
 ]
