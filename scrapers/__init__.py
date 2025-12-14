fa98b2ea90de9e3efc90dbd244a8f3880a3228d6
YtalloCosta <164099807+YtalloCosta@users.noreply.github.com>
2025-12-13 22:04:30 -0300

    Update __init__.py

--- scrapers/__init__.py
+++ scrapers/__init__.py
@@ -1,17 +1,18 @@
 from .pinnacle import PinnacleScraper
 from .stake import StakeScraper
 from .betano import BetanoScraper
-from .sportingbet import SportingbetScraper
+from .sportingbet import SportingBetScraper
 from .kto import KTOScraper
 from .bwin import BwinScraper
 from .xb1 import OneXBetScraper
 from .xb22 import TwentyTwoBetScraper
 
+
 SCRAPERS = [
     PinnacleScraper(),
     StakeScraper(),
     BetanoScraper(),
-    SportingbetScraper(),
+    SportingBetScraper(),
     KTOScraper(),
     BwinScraper(),
     OneXBetScraper(),
